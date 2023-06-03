78999894ad79a33513717cfaf37704cd05a48da4
Aiden Mayoros <aiden.mayoros@gmail.com>
2023-06-02 19:20:02 -0700

    Update account form has prefilled values of user

--- src/pages/ProfileView.jsx
+++ src/pages/ProfileView.jsx
@@ -14,29 +14,50 @@ import axios from 'axios';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import dayjs from 'dayjs';
 
 export default function ProfileView({ user, movies }) {
 	const [username, setUsername] = useState('');
 	const [password, setPassword] = useState('');
 	const [email, setEmail] = useState('');
-	const [birthdate, setBirthdate] = useState(null);
+	const [birthdate, setBirthdate] = useState(user ? formatedBirthdate() : null);
 	const [showErrorMessage, setShowErrorMessage] = useState(false);
 	const [errorMessage, setErrorMessage] = useState('');
 	const navigate = useNavigate();
 
+	console.log(birthdate);
 	console.log(user);
+
+	function padTo2Digits(num) {
+		return num.toString().padStart(2, '0');
+	}
+
+	function formatedBirthdate() {
+		const date = new Date(user.Birthday);
+		const year = date.getFullYear();
+		const month = date.getMonth() + 1;
+		const day = date.getDate();
+
+		return [padTo2Digits(month), padTo2Digits(day), year].join('/');
+	}
 
 	function handleSubmit() {
 		return;
 	}
 
 	return (
-		<Container sx={{ display: 'flex', justifyContent: 'space-between', p: 5 }} maxWidth={'100%'}>
-			<Box sx={{ display: 'flex', flexDirection: 'column', width: '50%', maxWidth: '450px' }}>
+		<Container sx={{ display: 'flex', justifyContent: 'center', p: 5 }} maxWidth={'100%'}>
+			<Box
+				sx={{
+					display: 'flex',
+					flexDirection: 'column',
+					width: '100%',
+					maxWidth: '450px',
+				}}>
 				<Typography variant='h5'>Update Account</Typography>
 				<Box component='form' onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
 					<Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-						<Grid item xs={12} md={6}>
+						<Grid item xs={12}>
 							<TextField
 								sx={{ width: '100%', maxWidth: '300px' }}
 								margin='normal'
@@ -46,10 +67,11 @@ export default function ProfileView({ user, movies }) {
 								name='username'
 								autoFocus
 								autoComplete='username'
+								value={user ? user.Username : 'Username'}
 								onChange={(e) => setUsername(e.target.value)}
 							/>
 						</Grid>
-						<Grid item xs={12} md={6}>
+						<Grid item xs={12}>
 							<TextField
 								sx={{ width: '100%', maxWidth: '300px' }}
 								margin='normal'
@@ -62,7 +84,7 @@ export default function ProfileView({ user, movies }) {
 								onChange={(e) => setPassword(e.target.value)}
 							/>
 						</Grid>
-						<Grid item xs={12} md={6}>
+						<Grid item xs={12}>
 							<TextField
 								sx={{ width: '100%', maxWidth: '300px' }}
 								margin='normal'
@@ -72,15 +94,16 @@ export default function ProfileView({ user, movies }) {
 								type='email'
 								id='email'
 								autoComplete='current-email'
-								onChange={(e) => setPassword(e.target.value)}
+								value={user ? user.Email : 'Email'}
+								onChange={(e) => setEmail(e.target.value)}
 							/>
 						</Grid>
-						<Grid item xs={12} md={6}>
+						<Grid item xs={12}>
 							<LocalizationProvider dateAdapter={AdapterDayjs}>
 								<DatePicker
 									sx={{ mt: 2, width: '100%', maxWidth: '300px' }}
 									label='Birthdate'
-									value={birthdate}
+									defaultValue={dayjs(birthdate)}
 									onChange={(newValue) => setBirthdate(newValue.$d)}
 								/>
 							</LocalizationProvider>
