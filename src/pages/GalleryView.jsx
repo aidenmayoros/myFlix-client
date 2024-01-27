805f03d8576a1eac660cf4914fdf824b03df83ae
Aiden Mayoros <aiden.mayoros@gmail.com>
2024-01-27 11:31:35 -0700

    adjusted styling and images show up.

--- src/pages/GalleryView.jsx
+++ src/pages/GalleryView.jsx
@@ -17,6 +17,8 @@ const Alert = React.forwardRef(function Alert(props, ref) {
 });
 
 export default function GalleryView({ gallery, addImage }) {
+	console.log(gallery);
+
 	const [file, setFile] = useState(null);
 	const [showLoadingSpinner, setShowLoadingSpinner] = useState(false);
 	const [alertMessage, setAlertMessage] = useState('');
@@ -88,36 +90,25 @@ export default function GalleryView({ gallery, addImage }) {
 			<Typography variant='h4' sx={{ m: 1, pt: 1 }}>
 				Images from S3 Bucket
 			</Typography>
-			<ImageList sx={{ width: '50%' }} cols={3}>
-				{itemData.map((item) => (
-					<ImageListItem key={item.img}>
-						<img
-							srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-							src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-							alt={item.title}
-							loading='lazy'
-						/>
-					</ImageListItem>
-				))}
-			</ImageList>
 			{showLoadingSpinner ? (
-				<Box sx={{ display: 'flex', justifyContent: 'center', mt: 50 }}>
+				<Box sx={{ display: 'flex' }}>
 					<CircularProgress size={100} />
 				</Box>
 			) : (
-				<form onSubmit={handleSubmit}>
-					<Input
-						type='file'
-						onChange={handleFileChange}
-						sx={{ p: 1, m: 1 }}></Input>
-					<Button
-						type='submit'
-						variant='contained'
-						startIcon={<CloudUploadIcon />}>
-						Upload File
-					</Button>
-				</form>
+				<div></div>
 			)}
+			<form onSubmit={handleSubmit}>
+				<Input
+					type='file'
+					onChange={handleFileChange}
+					sx={{ p: 1, m: 1 }}></Input>
+				<Button
+					type='submit'
+					variant='contained'
+					startIcon={<CloudUploadIcon />}>
+					Upload File
+				</Button>
+			</form>
 			<Snackbar
 				open={open}
 				autoHideDuration={6000}
@@ -130,54 +121,18 @@ export default function GalleryView({ gallery, addImage }) {
 					{alertMessage}
 				</Alert>
 			</Snackbar>
+			<ImageList cols={3} gap={18}>
+				{gallery.map((imageName) => (
+					<ImageListItem sx={{ width: 200, height: 200 }}>
+						<img
+							// src={`${MyFlixUrl}/view-image/${imageName}`}
+							src={`http://localhost:8080/view-image/${imageName}`}
+							alt={imageName}
+							loading='lazy'
+						/>
+					</ImageListItem>
+				))}
+			</ImageList>
 		</Container>
 	);
 }
-
-// 		img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-// 		title: 'Breakfast',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-// 		title: 'Burger',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-// 		title: 'Camera',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-// 		title: 'Coffee',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-// 		title: 'Hats',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-// 		title: 'Honey',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-// 		title: 'Basketball',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-// 		title: 'Fern',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-// 		title: 'Mushrooms',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-// 		title: 'Tomato basil',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-// 		title: 'Sea star',
-// 	},
-// 	{
-// 		img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-// 		title: 'Bike',
-// 	},
